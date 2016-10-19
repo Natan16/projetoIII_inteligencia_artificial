0b15f31fad1791ffa18342dc741a8c417f8f2c33
Natan16 <natanvianat16@gmail.com>
2016-10-19 17:28:41 -0200

    Class Users created

--- src/tabelas/Rating.java
+++ src/tabelas/Rating.java
@@ -1,5 +1,5 @@
 package tabelas;
 
 public class Rating {
-
+	
 }
--- src/tabelas/User.java
+++ src/tabelas/User.java
@@ -1,5 +1,33 @@
 package tabelas;
 
 public class User {
-
+ 	private char gender ;
+	private int age;
+	private int occupation;
+	private int zipcode ;
+	
+	public char getGender() {
+		return gender;
+	}
+	public void setGender(char gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getOccupation() {
+		return occupation;
+	}
+	public void setOccupation(int occupation) {
+		this.occupation = occupation;
+	}
+	public int getZipcode() {
+		return zipcode;
+	}
+	public void setZipcode(int zipcode) {
+		this.zipcode = zipcode;
+	}
 }
