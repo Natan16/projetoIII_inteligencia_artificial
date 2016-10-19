bd7eeb0b52434cafc6f882fb0c3939a11a94925e
Natan16 <natanvianat16@gmail.com>
2016-10-19 17:31:20 -0200

    Class Rating created

--- src/tabelas/Rating.java
+++ src/tabelas/Rating.java
@@ -1,5 +1,37 @@
 package tabelas;
 
 public class Rating {
-	
+	private int userID , movieID , rating , timestamp;
+
+	public int getUserID() {
+		return userID;
+	}
+
+	public void setUserID(int userID) {
+		this.userID = userID;
+	}
+
+	public int getMovieID() {
+		return movieID;
+	}
+
+	public void setMovieID(int movieID) {
+		this.movieID = movieID;
+	}
+
+	public int getRating() {
+		return rating;
+	}
+
+	public void setRating(int rating) {
+		this.rating = rating;
+	}
+
+	public int getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(int timestamp) {
+		this.timestamp = timestamp;
+	}
 }
